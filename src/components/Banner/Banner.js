bbe032c73bb8949f8662995368d42524be1adb61
Sachin Khati <khatisachin448@gmail.com>
2025-11-02 13:25:41 +0530

    make the banners for diff. devicess

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
-import {
-  bannerImgOne,
-  bannerImgTwo,
-  bannerImgThree,
-} from "../../assets/images";
+import { bannerImgOne, bannerImgTwo, bannerImgThree } from "../../assets/images";
 import Image from "../designLayouts/Image";
 
 const Banner = () => {
-  const [dotActive, setDocActive] = useState(0);
+  const [activeDot, setActiveDot] = useState(0);
+
+  const images = [bannerImgOne, bannerImgTwo, bannerImgThree];
+
   const settings = {
     dots: true,
     infinite: true,
@@ -17,80 +16,38 @@ const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    beforeChange: (prev, next) => {
-      setDocActive(next);
-    },
+    beforeChange: (_, next) => setActiveDot(next),
+
     appendDots: (dots) => (
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "7%",
-          transform: "translateY(-50%)",
-        }}
-      >
-        <ul style={{ margin: "0px" }}> {dots} </ul>
+      <div className="absolute top-1/2 left-5 -translate-y-1/2">
+        <ul className="m-0 p-0">{dots}</ul>
       </div>
     ),
+
     customPaging: (i) => (
       <div
-        style={
-          i === dotActive
-            ? {
-                width: "30px",
-                color: "#262626",
-                borderRight: "3px #262626 solid",
-                padding: "8px 0",
-                cursor: "pointer",
-              }
-            : {
-                width: "30px",
-                color: "transparent",
-                borderRight: "3px white solid",
-                padding: "8px 0",
-                cursor: "pointer",
-              }
-        }
+        className={`w-8 cursor-pointer py-2 ${
+          i === activeDot ? "text-black border-r-4 border-black" : "text-transparent border-r-4 border-white"
+        }`}
       >
         0{i + 1}
       </div>
     ),
+
     responsive: [
       {
         breakpoint: 576,
         settings: {
-          dots: true,
           appendDots: (dots) => (
-            <div
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "2%",
-                transform: "translateY(-50%)",
-              }}
-            >
-              <ul style={{ margin: "0px" }}> {dots} </ul>
+            <div className="absolute top-1/2 left-2 -translate-y-1/2">
+              <ul>{dots}</ul>
             </div>
           ),
           customPaging: (i) => (
             <div
-              style={
-                i === dotActive
-                  ? {
-                      width: "25px",
-                      color: "#262626",
-                      borderRight: "3px #262626 solid",
-                      cursor: "pointer",
-                      fontSize: "12px",
-                    }
-                  : {
-                      width: "25px",
-                      color: "transparent",
-                      borderRight: "3px white solid",
-                      cursor: "pointer",
-                      fontSize: "12px",
-                    }
-              }
+              className={`w-6 text-xs cursor-pointer ${
+                i === activeDot ? "text-black border-r-4 border-black" : "text-transparent border-r-4 border-white"
+              }`}
             >
               0{i + 1}
             </div>
@@ -99,24 +56,17 @@ const Banner = () => {
       },
     ],
   };
+
   return (
     <div className="w-full bg-white">
       <Slider {...settings}>
-        <Link to="/offer">
-          <div>
-            <Image imgSrc={bannerImgOne} />
-          </div>
-        </Link>
-        <Link to="/offer">
-          <div>
-            <Image imgSrc={bannerImgTwo} />
-          </div>
-        </Link>
-        <Link to="/offer">
-          <div>
-            <Image imgSrc={bannerImgThree} />
-          </div>
-        </Link>
+        {images.map((src, index) => (
+          <Link key={index} to="/offer">
+            <div>
+              <Image imgSrc={src} />
+            </div>
+          </Link>
+        ))}
       </Slider>
     </div>
   );
